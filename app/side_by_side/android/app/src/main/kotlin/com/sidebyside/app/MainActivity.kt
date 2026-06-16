4ee1c5fc82675e8594f03b230e5f99db45deef8e
micaelesales19 <micaelesales19@gmail.com>
2026-06-16 11:04:55 -0300

    ataulizado

--- app/side_by_side/android/app/src/main/kotlin/com/sidebyside/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.sidebyside.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- app/side_by_side/lib/utils/pwa_install_page.dart
+++
@@ -1,1 +0,0 @@
-export 'pwa_stub.dart' if (dart.library.html) 'PwaInstallPage.dart';
